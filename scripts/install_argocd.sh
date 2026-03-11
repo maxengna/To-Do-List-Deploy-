3153aabb107229085ece08b21463e5da69a6a363
maxengna <phanupong.w2019@gmail.com>
2026-03-11 13:58:30 +0700

    fix Sensitive key

--- scripts/install_argocd.sh
+++ scripts/install_argocd.sh
@@ -1,11 +1,12 @@
 kubectl create ns argocd || echo "Namespace argocd already exists"
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || echo "ArgoCD already installed"
 minikube service argocd-server -n argocd || echo "ArgoCD service already exists"
-
+# wait for 10 seconds
+sleep 10
 kubectl port-forward -n argocd svc/argocd-server 8083:443 || echo "ArgoCD port-forward already exists"
-
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-
+sleep 10
+# kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+ 
 
 # quick tunnel argocd in cloudflare
 # cloudflare tunnel --url http://localhost:8083 --no-tls-verify
