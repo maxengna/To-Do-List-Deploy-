acb8ccc7c1c7fe0ee00bc03ddac817437bf36c75
maxengna <phanupong.w2019@gmail.com>
2026-03-12 15:00:59 +0700

    change endpoint tempo

--- scripts/install_argocd.sh
+++ scripts/install_argocd.sh
@@ -2,9 +2,9 @@ kubectl create ns argocd || echo "Namespace argocd already exists"
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml || echo "ArgoCD already installed"
 minikube service argocd-server -n argocd || echo "ArgoCD service already exists"
 # wait for 10 seconds
-sleep 10
-kubectl port-forward -n argocd svc/argocd-server 8083:443 || echo "ArgoCD port-forward already exists"
-sleep 10
+# sleep 10
+# kubectl port-forward -n argocd svc/argocd-server 8083:443 || echo "ArgoCD port-forward already exists"
+# sleep 10
 # kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
  
 
